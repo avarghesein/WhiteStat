67fe951ef21e6b1ef6aa3747170b7ede946d0b1f
avarghesein <10294917+avarghesein@users.noreply.github.com>
2021-02-13 23:14:03 +0530

    V8

--- WhiteStat/WhitestatAutoBuildPublish.sh
+++ WhiteStat/WhitestatAutoBuildPublish.sh
@@ -5,10 +5,10 @@ cd ./Analyzer/UX/
 npm run build
 cd ../..
 
-docker image rm $(docker image list | grep "avarghesein/whitestat" | grep v7 |  tr -s " " | cut -d " " -f 3)
+docker image rm $(docker image list | grep "avarghesein/whitestat" | grep v8 |  tr -s " " | cut -d " " -f 3)
 docker image rm $(docker image list | grep none |  tr -s " " | cut -d " " -f 3)
 
-docker build -f Dockerfile.armhf -t avarghesein/whitestat:v7_armhf .
-docker build -f Dockerfile -t avarghesein/whitestat:v7 .
-docker push avarghesein/whitestat:v7_armhf
-docker push avarghesein/whitestat:v7
+docker build -f Dockerfile.armhf -t avarghesein/whitestat:v8_armhf .
+docker build -f Dockerfile -t avarghesein/whitestat:v8 .
+docker push avarghesein/whitestat:v8_armhf
+docker push avarghesein/whitestat:v8
--- WhiteStat/WhitestatAutoDockerDeploy.sh
+++ WhiteStat/WhitestatAutoDockerDeploy.sh
@@ -2,14 +2,15 @@
 
 docker container stop whitestatpi
 docker container rm whitestatpi
-docker image rm $(docker image list | grep "avarghesein/whitestat" | grep v7 |  tr -s " " | cut -d " " -f 3)
+docker image rm $(docker image list | grep "avarghesein/whitestat" | grep v8 |  tr -s " " | cut -d " " -f 3)
 
 docker run --name whitestatpi --restart always \
---name hostnet --network host --privileged \
+--network host --privileged \
 --env TZ="Asia/Calcutta" \
 --env ROLE="MONITOR|ANALYZER" \
+--env HOST_INTERFACE="eth0" \
 --env MONITOR_URL=":888" \
 --env ANALYZER_PORT=777 \
---env DATA_STORE="/mnt/whitestat/config/" \
+--env DATA_STORE="/mnt/whitestat/" \
 --mount type=bind,source="/home/pi/whitestat/",target="/mnt/whitestat/"  \
--d avarghesein/whitestat:v7_armhf
+-d avarghesein/whitestat:v8_armhf
