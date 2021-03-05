b5e1ca7bf2d755dfa6ba597161787c6658a84377
AVarghese <10294917+avarghesein@users.noreply.github.com>
2021-03-05 16:15:28 +0530

    Auto Build Publish Windows Native Container through Powershell

--- WhiteStat/WhitestatAutoBuildPublish.ps1
+++ WhiteStat/WhitestatAutoBuildPublish.ps1
@@ -3,8 +3,8 @@ cd ./Analyzer/UX/
 npm run build
 cd ../..
 
-docker image rm $(docker image list | grep "avarghesein/whitestat" | grep v9 |  tr -s " " | cut -d " " -f 3)
-docker image rm $(docker image list | grep none |  tr -s " " | cut -d " " -f 3)
+docker image rm $($(docker image ls).Split([Environment]::NewLine) | %{ $_ -replace '\s+', ' '} | Select-String -Pattern "v10_win64"  | %{ $_.ToString().split(" ")[2] })
+docker image rm $($(docker image ls).Split([Environment]::NewLine) | %{ $_ -replace '\s+', ' '} | Select-String -Pattern "none"  | %{ $_.ToString().split(" ")[2] })
 
 docker build -f Dockerfile.WIN64 -t avarghesein/whitestat:v10_win64 .
 docker push avarghesein/whitestat:v10_win64
