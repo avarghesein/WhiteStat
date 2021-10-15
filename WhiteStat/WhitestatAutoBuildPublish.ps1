58b4af782c181b7c19b1956ce688857307b3b401
AVarghese <10294917+avarghesein@users.noreply.github.com>
2021-10-15 15:36:30 +0530

    Windows Docker Image Updates V11

--- WhiteStat/WhitestatAutoBuildPublish.ps1
+++ WhiteStat/WhitestatAutoBuildPublish.ps1
@@ -3,8 +3,8 @@ cd ./Analyzer/UX/
 npm run build
 cd ../..
 
-docker image rm $($(docker image ls).Split([Environment]::NewLine) | %{ $_ -replace '\s+', ' '} | Select-String -Pattern "v10_win64"  | %{ $_.ToString().split(" ")[2] })
+docker image rm $($(docker image ls).Split([Environment]::NewLine) | %{ $_ -replace '\s+', ' '} | Select-String -Pattern "v11_win64"  | %{ $_.ToString().split(" ")[2] })
 docker image rm $($(docker image ls).Split([Environment]::NewLine) | %{ $_ -replace '\s+', ' '} | Select-String -Pattern "none"  | %{ $_.ToString().split(" ")[2] })
 
-docker build -f Dockerfile.WIN64 -t avarghesein/whitestat:v10_win64 .
-docker push avarghesein/whitestat:v10_win64
+docker build -f Dockerfile_v10+.WIN64 -t avarghesein/whitestat:v11_win64 .
+docker push avarghesein/whitestat:v11_win64
